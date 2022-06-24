24a3388f92f2298e125595a1c7231dcc6ed93fe2
nushrath2001 <nushrathjahan3@gmail.com>
2022-06-24 21:20:32 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,31 @@
+// string 
+var intro= "Nushrath.j";
+console.log(typeof intro , intro);
+
+
+// boolean
+var conf = false;
+console.log(typeof conf,conf);
+
+// number
+var a = 20;
+console.log(typeof a, a);
+
+
+// undefined 
+var prog = undefined;
+console.log(typeof prog ,prog)
+
+// symbol
+var texture =Symbol();
+console.log(typeof texture,texture)
+
+/ null
+var object = null;
+console.log(typeof object,object);
+
+// corceian
+var Name = "nushrath";
+var  a = 20;
+var sum = Name+a;
+console.log(typeof sum,sum);
